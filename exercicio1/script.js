6dc1cd5fbca0559b594e4dbba16c2eb206c44c9c
BarbaraAmaral27 <barbaramamaral@gmail.com>
2022-08-30 20:10:26 -0300

    exercicios usando operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+let numero = Number (prompt("Digite um número par:"))// ou Number(numero) para converter minha váriavel para numero
+
+console.log(numero)
+
+let divisao = numero%2
+console.log(divisao)
+
+//b- Toda vez que o usuário digitar um número par na divisão por 2, o resto será sempre 0, pois está tentando dividir um número par por 2 que também é par.
+//c- Caso o usuário digite um número ímpar o resto da divisão será 1.
+
---
+++ exercicio2/script.js
@@ -0,0 +1,15 @@
+let numero1 = Number (prompt("Digite o primeiro número:"))
+let numero2 = Number (prompt("Digite o primeiro número:"))
+
+let maiorNumero = numero1 > numero2
+console.log(maiorNumero)
+
+let numeroIgual = numero1 === numero2
+console.log(numeroIgual)
+
+let divPrimeiro = numero1 % numero2 === 0
+console.log(divPrimeiro)
+
+let divSegundo = numero2 % numero1 === 0
+console.log(divSegundo)
+
